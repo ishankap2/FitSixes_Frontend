4d15e5ca0951e2e8349b315224b50a99e9f18d09
ishankap2 <ishankap2>
2017-09-22 07:09:26 -0700

    create team added

--- src/app/admin/create-match/create-match.component.ts
+++ src/app/admin/create-match/create-match.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { CreateMatchService } from '../../services/create-match.service';
+import {TeamServiceService} from '../../services/team-service.service';
 
 @Component({
   selector: 'app-create-match',
   templateUrl: './create-match.component.html',
-  styleUrls: ['./create-match.component.css']
+  styleUrls: ['./create-match.component.css'],
+  providers:[CreateMatchService,TeamServiceService]
 })
 export class CreateMatchComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private matchService: CreateMatchService,
+    private teamService: TeamServiceService
+  ) { }
+
+  team1id:any; 
+  team2id:any;
+  overs: number;
+  balls: number;
+
+  teams : any;
 
   ngOnInit() {
+    this.teamService.getAllTeams()
+    .subscribe(
+      data =>{ this.teams= data},
+      error => alert(error)     
+      );
+  }
+
+  createMatch(){
+    console.log(this.team1id,this.team2id,this.overs,this.balls)
   }
 
 }
