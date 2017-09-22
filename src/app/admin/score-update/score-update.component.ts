b09389fa5aef3d4750fc2c711d31fb87c4a33fc4
Udara Sampath Bandara <usbgalle@gmail.com>
2017-09-22 06:21:26 +0530

    succesfull score updated

--- src/app/admin/score-update/score-update.component.ts
+++ src/app/admin/score-update/score-update.component.ts
@@ -7,50 +7,154 @@ import { ScoreUpdateService } from '../../services/score-update.service';
   styleUrls: ['./score-update.component.css']
 })
 export class ScoreUpdateComponent implements OnInit {
-	score:any = {}
+	data:any = {}
 	bowlingscore:any ={}
-	battingscore:any={}
-  matchDetails:any={}
+	battingscore:any ={}
+  matchDetails:any ={}
+  over_num:number=0;
+  ball_num:number=1;
+  players=[]
   tempary=[]
-  data:any={}
+  tempScores = [];
+  scoreStr:string = "";
+  totScore:number=0;
+  totalScoreWicketsStr:string = "0/0";
+  currentOvers:string = "0.0";
   constructor(private scoreUpdateService:ScoreUpdateService) { }
 
   ngOnInit() {
-      //getMatchDetails(){
-        this.matchDetails={'matchId':1,'name':'Vertusa1VSVertusa2','groundId':1,'groundName':'A','team1Id': 1, 'team1Name':'Vertusa Team 1','team2Id': 2, 'team2Name': 'Vertusa Team 2', 'roundId': 1, 'InningId': 1, 'battingTeamId': 1, 'battingTeamName': 'Vertusa Team 1', 'target': null }        
-       
-
-      //}
-        this.battingscore={'teamId':this.matchDetails.team1Id,'teamName':this.matchDetails.team1Name}
-        this.bowlingscore={'teamId':this.matchDetails.team2Id,'teamName':this.matchDetails.team2Name,'extra':0}
-         this.battingscore.players=[
-        {'playerId':1,'name':'pra'},{'playerId':2,'name':'nimal'},{'playerId':3,'name':'kamal'},{'playerId':4,'name':'sugath'},{'playerId':5,'name':'kusal'}
-        ]
-        this.bowlingscore.players=[
-        {'playerId':6,'name':'udara'},{'playerId':7,'name':'prabhath'},{'playerId':8,'name':'chaml'},{'playerId':9,'name':'perrera'},{'playerId':10,'name':'ajith'}
-        ]
-        this.bowlingscore.ball_type="valid"
-        this.bowlingscore.runs=0
+      this.getMatchDetails()        
+        
   }
 
   updateBallByball(){
-        if(this.bowlingscore.ball_type=="no_ball" || this.bowlingscore.ball_type=="wide"){
-          this.bowlingscore.extra++;
+        //this.players[0][0].score = "aaaaaaaaaaaaaaaaaa";
+
+        this.bowlingscore.over=Number(""+this.over_num+"."+this.ball_num)
+        this.bowlingscore.currentBall=Number("0."+this.ball_num)
+        if(this.data.wicket){
+          this.bowlingscore.isWicket=1
         }
-        if(this.bowlingscore.ballStatus==2 || this.bowlingscore.ballStatus==3){
-          this.bowlingscore.extra=this.bowlingscore.extra+this.battingscore.runs;
+        if(this.data.run_out){
+          this.bowlingscore.isRunOut=1
+        }
+        if(this.data.ball_type=="valid"){
+          this.bowlingscore.isValidBall=1
+          this.ball_num++;
+          if(this.ball_num > this.matchDetails.ballsPerOvers){
+            this.over_num++;
+            this.ball_num=1;
+          }
+        }
+        if(this.data.ball_type=="no_ball"){
+          this.bowlingscore.extras++;
+          this.bowlingscore.runs++;
+          this.bowlingscore.isNoBall=1
+          
+        }
+        if (this.data.ball_type=="wide"){
+          this.bowlingscore.extras++;
+          this.bowlingscore.runs++;
+          this.bowlingscore.isWide=1
+        }
+        if(this.data.bye){
+          //this.bowlingscore.ballStatus="Bye";
+          this.bowlingscore.extras=this.bowlingscore.extras+this.battingscore.runs;
           this.battingscore.runs=0;
+        }
+        if(this.battingscore.runs==4){
+          this.battingscore.isFour=1;
+        }
+        if(this.battingscore.runs==6){
+          this.battingscore.isSix=1;
+        }
+        if(this.bowlingscore.extras==0 && this.battingscore.runs==0){
+          this.battingscore.isDot=1;
         }
   	    this.scoreUpdateService.updateScore(this.battingscore,this.bowlingscore)
         .subscribe(
-            data => {
-              console.log(data);
-              },
-            error => {
-              console.log(error._body);
-            },
-            ()=> console.log("Finished")
-        );
+                         res=>{
+                           console.log(res);    
+                         },
+                         err=> {}
+                         ),this.getPlayerScore();
+                            this.getBowlerResult();
+                            this.setDefault();        
+  }
+
+  getPlayerScore(){
+     this.scoreUpdateService.getPlayerScore(this.matchDetails.matchId,this.battingscore.playerId,this.battingscore.teamId)
+        .subscribe(
+                         res=>{
+                           this.tempScores = res.playerScore;
+                           for(let sc of this.tempScores){
+                             this.scoreStr = this.scoreStr + sc+"  ";
+                             this.totScore = this.totScore + sc;
+                           }
+                           for(let player of this.players[0]){
+                             if(player.playerId == this.battingscore.playerId){
+                               player.score = this.scoreStr;
+                               player.total = this.totScore;
+                             }
+                           }
+                           this.totalScoreWicketsStr = res.matchResult.total+"/"+res.matchResult.wickets;
+                           this.currentOvers = res.matchResult.currentOvers;
+                           console.log(res);
+                         },
+                         err=> {}
+                         );
+  }
+
+  getBowlerResult(){
+     this.scoreUpdateService.getBowlerResult(this.matchDetails.matchId,this.bowlingscore.bowlerId,this.battingscore.teamId)
+        .subscribe(
+                         res=>{
+                           for(let player of this.players[1]){
+                             if(player.playerId == this.bowlingscore.bowlerId){
+                               player.bowlerResult = res.bowlerResult;
+                             }
+                           }
+                           console.log(res);
+                         },
+                         err=> {}
+                         );
+  }
+
+  getMatchDetails(){
+     this.scoreUpdateService.getMatchDetails()
+     .subscribe(
+                         res=>{
+                            this.matchDetails=res;
+                            this.battingscore={'matchId':this.matchDetails.matchid,'teamId':this.matchDetails.Teams[0].teamid,'teamName':this.matchDetails.Teams[0].teamName}
+                            this.bowlingscore={'matchId':this.matchDetails.matchid,'teamId':this.matchDetails.Teams[1].teamid,'teamName':this.matchDetails.Teams[1].teamName,'extras':0}
+                            this.setDefault()
+                            this.players[0]=[];
+                            this.players[1]=[];
+                            for(let player of this.matchDetails.Teams[0].Players){
+                              let newVal = {
+                                playerId:player.playerId,
+                                name:player.name,
+                                score:'',
+                                total:0,
+                                bowlerResult:{}
+                              };
+                              this.players[0].push(newVal);
+                            }
+                            for(let player of this.matchDetails.Teams[1].Players){
+                              let newVal = {
+                                playerId:player.playerId,
+                                name:player.name,
+                                score:'',
+                                total:0,
+                                bowlerResult:{}
+                              };
+                              this.players[1].push(newVal);
+                            }
+                            console.log(this.players[0]);
+                            console.log(this.players[1]);
+                         },
+                         err=> {}
+                         );
   }
 
   swapInnig(){
@@ -58,10 +162,33 @@ export class ScoreUpdateComponent implements OnInit {
     this.battingscore=this.bowlingscore
     this.bowlingscore=this.tempary
     this.tempary=[];
+    this.tempary=this.players[0]
+    this.players[0]=this.players[1]
+    this.players[1]=this.tempary
+    this.tempary=[];
+
   }
 
-  // getTeams(){
-  // 	this.scoreUpdateService.getPlayingTeams();
-  // }
+  setDefault(){
+        this.data.ball_type="valid"
+        this.bowlingscore.runs=0
+        this.battingscore.runs=0
+        this.bowlingscore.isNoBall=0
+        this.bowlingscore.isWide=0
+        this.bowlingscore.isWicket=0
+        this.bowlingscore.isRunOut=0
+        this.bowlingscore.isValidBall=0
+        this.bowlingscore.extras=0
+        this.battingscore.isFour=0;
+        this.battingscore.isSix=0;
+        this.battingscore.isDot=0;
+        this.data.bye=false;
+        this.data.run_out=false
+        this.data.wicket=false
+        this.scoreStr = "";
+        this.totScore = 0;
+        
+  }
+
 
 }
--- src/app/services/score-update.service.ts
+++ src/app/services/score-update.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 
 export class BattingScore {
     teamId:number;
@@ -25,12 +26,33 @@ export class ScoreUpdateService {
 
   constructor(private http:Http) { }
 
-       updateScore(par_1:any,par_2:any){
-           console.log({par_1,par_2});
-        return this.http.post('localhost:8000/', { battingscore: par_1,bowlingscore:par_2}); 
+       updateScore(battingScore:BattingScore,bowlingscore:any): Observable<any>{
+        return this.http.post('http://fitsixes.azurewebsites.net/api/bowler/addNewBall', {batting:battingScore,bowler:bowlingscore})
+        .map((res:Response) => res.json())
+                        .catch((error:any)=>Observable.throw(error.json().error_description || 'Server error'));
     	}
 
-    getPlayingTeams(){
+        getMatchDetails(): Observable<any>{
+        return this.http.get('http://localhost:8000/json')
+                        // ...and calling .json() on the response to return data
+                         .map((res:Response) => res.json())
+                         //...errors if any
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+        }
 
-    }
+        getPlayerScore(matchId:any,playerId:any,teamId:any){
+            return this.http.post('http://localhost:8000/player',{matchId:matchId,playerId:playerId,teamId:teamId})
+                        // ...and calling .json() on the response to return data
+                         .map((res:Response) => res.json())
+                         //...errors if any
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));   
+        }
+
+        getBowlerResult(matchId:any,playerId:any,teamId:any){
+            return this.http.post('http://localhost:8000/player',{matchId:matchId,playerId:playerId,teamId:teamId})
+                        // ...and calling .json() on the response to return data
+                         .map((res:Response) => res.json())
+                         //...errors if any
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error'));   
+        }
 }
